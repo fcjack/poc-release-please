f9c35a4cf73b321dc5dd8c665a3aeadfb1bb644e
Jackson Coelho <fcjack@gmail.com>
2024-08-09 11:27:18 +0200

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fcjack/poc-release-please
+
+go 1.22.6
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// This is a comment
+	// This is another comment
+	// This is a third comment
+	println("Hello, World!")
+}
